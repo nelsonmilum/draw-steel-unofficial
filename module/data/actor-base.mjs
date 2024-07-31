37186c9f02b33ae22d56a06dd23310187f4acaea
Nelson Milum <github.yearbook188@passmail.net>
2024-07-30 23:50:12 -0500

    Verified no foundry v12 warnings when creating items or actors, good clean fun.

--- module/data/actor-base.mjs
+++ module/data/actor-base.mjs
@@ -9,7 +9,7 @@ export default class DrawSteelActorBase extends foundry.abstract.TypeDataModel {
       value: new fields.NumberField({ ...requiredInteger, initial: 10, min: 0 }),
       max: new fields.NumberField({ ...requiredInteger, initial: 10 })
     });
-    schema.power = new fields.SchemaField({
+    schema.recoveries = new fields.SchemaField({
       value: new fields.NumberField({ ...requiredInteger, initial: 5, min: 0 }),
       max: new fields.NumberField({ ...requiredInteger, initial: 5 })
     });
